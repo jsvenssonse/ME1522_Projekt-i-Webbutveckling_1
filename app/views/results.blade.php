b3374028d947e883f809bb25e369bc67de6edd0f
Jesper Kungen <j.svensson.se@gmail.com>
2015-04-21 14:33:57 +0100

    search GOOD

--- app/views/results.blade.php
+++ app/views/results.blade.php
@@ -1,18 +1,21 @@
 @extends('layouts.master')
 @section('content')
 
+
 <style>
 h5  {color:white}
 </style>
-<br>
+<br>	
+
 	<div class="large-4 columns">
 	    <div class="large-12 columns">
       <h5>Välj ledig stuga</h5>
       {{Form::open(array('url' => 'verification', 'method' => 'POST')) }}
       @for ($i=0; $i < count($houses); $i++)
-      {{ Form::checkbox('checkbox1', $houses[$i]->name) }}
-      <!--<input id="checkbox1" type="checkbox" value="checkbox1"><label for="checkbox1">{{$houses[$i]->name;}}</label>-->
+      	{{Form::label($houses[$i][0]->name)}}
+     	 {{ Form::checkbox('checkbox1', '') }}
       @endfor
+
 {{Form::submit('Bekräfta bokning', array('class' => 'button small ')) }}
 {{Form::close() }}
     </div>

fc19537e627cef58027496dd6c379bb2c608828d
viktor fredriksson <viktorfredriksson@live.com>
2015-04-29 13:15:42 +0200

    pussssshhhh

--- app/views/prices.blade.php
+++ app/views/prices.blade.php
@@ -6,15 +6,36 @@
 </head>
 
 <body>
+<div class="row table">
+<table>
+  <thead>
+    <tr>
+      <th width="200">Datum</th>
+      <th>Stugor</th>
+      <th width="150">Dygn</th>
+      <th width="150">Vecka</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>30/5-13/6, 15/8-29/8</td>
+      <td>Stuga 1 - 15</td>
+      <td>500 kr</td>
+      <td>3000 kr</td>
+    </tr>
+    <tr>
+      <td>13/6-15/8</td>
+      <td>Stuga 1 - 15</td>
+      <td>600 kr</td>
+      <td>3600 kr</td>
+    </tr>
+  </tbody>
+</table>
+</div>
+</body>
 
 
-
-
-
-
-
-
-
+@stop
 
 
 
